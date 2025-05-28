b35b202f8b0a093baceddd876b75b5670a67ef11
Ezzayer Amine <ae@localhost.localdomain>
2025-05-28 12:02:07 +0100

    chore: add fish config file (linux)

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+function activate
+    source ~/.env/$argv[1]/bin/activate.fish
+end
+
+function mkvenv
+    python -m venv ~/.env/$argv[1]
+    activate $argv[1]
+end
