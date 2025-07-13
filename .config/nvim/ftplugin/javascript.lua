0982623d08787f82a17ecd2085acc8facd02b0fb
Ezzayer Amine <aminezzayer.ing@gmail.com>
2025-07-13 12:26:28 +0100

    feat: nvim filetype configs

---
+++ .config/nvim/ftplugin/javascript.lua
@@ -0,0 +1,21 @@
+-- Set Python to use 4 spaces
+vim.bo.tabstop = 4         -- How many columns a tab counts for
+vim.bo.softtabstop = 4     -- Number of columns inserted with Tab
+vim.bo.shiftwidth = 4      -- Number of columns for autoindent
+vim.bo.expandtab = true    -- Convert tabs to spaces
+vim.opt.smartindent = true -- Smart auto-indenting
+
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
+--   callback = function()
+--     vim.lsp.buf.format({ async = false }) -- Force sync formatting
+--   end,
+-- })
+
+-- Enable Tree-sitter based folding
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
---
+++ .config/nvim/ftplugin/sql.lua
@@ -0,0 +1,13 @@
+vim.bo.tabstop = 4         -- How many columns a tab counts for
+vim.bo.softtabstop = 4     -- Number of columns inserted with Tab
+vim.bo.shiftwidth = 4      -- Number of columns for autoindent
+vim.bo.expandtab = true    -- Convert tabs to spaces
+vim.opt.smartindent = true -- Smart auto-indenting
+
+-- Enable Tree-sitter based folding
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
